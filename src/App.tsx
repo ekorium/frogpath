aa4cc34912a22a6912cd4bca71e3d49a97feae1c
ekorium <231641293+ekorium@users.noreply.github.com>
2025-11-12 18:14:35 +0100

    feat: add interactive map

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,18 @@
+import { useEffect, useRef } from "react";
+import { Map } from "maplibre-gl";
+import "maplibre-gl/dist/maplibre-gl.css";
+
 export default function App() {
-  return <></>;
+  const map = useRef<Map>(null);
+
+  useEffect(() => {
+    map.current = new Map({
+      container: "map",
+      style: 'https://tiles.openfreemap.org/styles/liberty',
+      center: [0, 0],
+      zoom: 0,
+    });
+  }, []);
+
+  return <div id="map"></div>;
 }
